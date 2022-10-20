aacf56c3cef51a7f6dc5f2bfb6b24fff39f0b2df
Askin <asknkskk@gmail.com>
2022-10-20 23:20:52 +0300

    icons added

--- src/pages/Dashboard/components/BlogCard.jsx
+++ src/pages/Dashboard/components/BlogCard.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { MdFavorite, MdVisibility } from 'react-icons/md';
+import { BsChatLeft } from 'react-icons/bs';
 
 const AllBlogs = ({ blog }) => {
-  console.log(blog);
   return (
-    <div className="flex flex-col border border-green-400 max-h-80 ">
+    <div className="flex flex-col border border-green-400 max-h-80 max-w-xs">
       <div className="max-h-44 max-w-xs ">
         <img
           src={blog?.image}
           alt="blog"
-          className=" object-contain h-full mx-auto"
+          className=" object-contain h-full w-full"
         />
       </div>
       <div className="flex flex-col">
@@ -20,20 +21,24 @@ const AllBlogs = ({ blog }) => {
           : blog?.content.slice(0, 75) + "..."}</p>
       </div>
       <div className="flex ">
-        <img src="" alt="profile-img" />
+        <img src={blog?.author?.image} alt="profile-img" />
         <p>{blog?.author}</p>
       </div>
-      <div className="flex gap-x-1">
-        <div className="flex">
-          <p>icon</p>
-          <p>{blog?.likes}</p>
+      <div className="flex gap-x-3">
+        <div className="flex items-center gap-x-1">
+          <span >
+            <MdFavorite className="text-red-800"/>
+            </span>
+          <p >{blog?.likes}</p>
         </div>
-        <div className="flex">
-          <p>icon</p>
+        <div className="flex items-center gap-x-1">
+         <span><MdVisibility/></span>
           <p>{blog?.post_views}</p>
         </div>
-        <div className="flex">
-          <p>icon</p>
+        <div className="flex items-center gap-x-1">
+          <span >
+            <BsChatLeft />
+            </span>
           <p>{blog?.comment_count}</p>
         </div>
       </div>
